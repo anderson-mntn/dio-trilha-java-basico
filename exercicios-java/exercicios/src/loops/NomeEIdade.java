5da92e0346c5522f598c9b0909af9ad23067375b
anderson amantino <andersonamantino@gmail.com>
2023-01-13 13:41:28 -0300

    Exercício finalizado

--- exercicios-java/exercicios/src/loops/NomeEIdade.java
+++ exercicios-java/exercicios/src/loops/NomeEIdade.java
@@ -9,11 +9,20 @@ public class NomeEIdade {
         String nome;
         int idade;
 
-        System.out.println("Nome: ");
-        nome = scan.next();
-        System.out.println("Idade: ");
-        idade = scan.nextInt();
+        while(true){
+
+            System.out.println("Nome: ");
+            nome = scan.next();
+            if(nome.equals("0")) break; //Digitar 0 para encerrar o recebimento de dados dos usuários
+            
+            System.out.println("Idade: ");
+            idade = scan.nextInt();
+            System.out.println("Usuário cadastrado! Seu nome é " + nome + ", e sua idade é " + idade + "!");
+            System.out.println("Próximo usuário...");
+        }
+
+        //System.out.println("Seu nome é: " + nome + " e sua idade é: " + idade);
         scan.close();
-        System.out.println("Seu nome é: " + nome + " e sua idade é: " + idade);
+        System.out.println("Programa Encerrado.");
     }
 }
