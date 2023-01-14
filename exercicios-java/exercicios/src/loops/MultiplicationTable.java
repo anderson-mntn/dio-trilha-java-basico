720853932bbff7707683db834dc0592e17d39965
anderson amantino <andersonamantino@gmail.com>
2023-01-14 15:22:28 -0300

    MultiplicationTable first commit

--- exercicios-java/exercicios/src/loops/MultiplicationTable.java
+++ exercicios-java/exercicios/src/loops/MultiplicationTable.java
@@ -1,5 +1,37 @@
 package loops;
+import java.util.Scanner;
+
+/*
+Desenvolva um gerador de tabuada,
+capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10.
+O usuário deve informar de qual numero ele deseja ver a tabuada. A saída deve ser conforme o exemplo abaixo: 
+Tabuada de 5:
+5 X 1 = 5
+5 X 2 = 10
+...
+5 X 10 = 50
+
+
+Make a multiplication table, that can show his multiplations of a int number
+between 1 and 10.
+The user should insert the number that will be multiplied. The output must be:
+Number 5 mult:
+5 X 1 = 5
+5 X 2 = 10
+...
+5 X 10 = 50
+*/
 
 public class MultiplicationTable {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Insira um numero para exibir sua tabuada: ");
+        int tabuada = scan.nextInt();
+
+        System.out.println("Tabuada do " + tabuada + ":");
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(tabuada + " X " + i + " = " + tabuada  * i);  
+        }
+        scan.close();
+    }
 }
