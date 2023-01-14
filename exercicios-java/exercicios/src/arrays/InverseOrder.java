e763466345131a8b34a0988d4576c45065136556
anderson amantino <andersonamantino@gmail.com>
2023-01-14 16:30:47 -0300

    Exercise finished

--- exercicios-java/exercicios/src/arrays/InverseOrder.java
+++ exercicios-java/exercicios/src/arrays/InverseOrder.java
@@ -9,13 +9,21 @@ public class InverseOrder {
     public static void main(String[] args) {
         //Temos que declarar tipo dos elementos que estarão dentro do array.
         int[] vetor = {3, 6, 9, 12, -2, -4, 72, 36};
-        int count = 0;
-
+        
         //System.out.println(vetor.length);
-
-        while(count < (vetor.length -1)){
-            System.out.println(vetor[count]);
+        
+        System.out.print("Vetor: ");
+        int count = 0;
+        while(count < (vetor.length)){
+            System.out.print(vetor[count] + " ");
             count++;
         }
+        System.out.print("\nVetor Inverso: ");
+        for (int i = (vetor.length - 1); i >= 0; i--) { //i recebe array.lenght e será decrementado
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.print("\n");
+
+
     }
 }
