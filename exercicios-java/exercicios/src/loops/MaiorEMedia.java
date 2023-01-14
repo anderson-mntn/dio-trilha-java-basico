8d28da6ffd30139bfed360494ba9b7e462a7f897
anderson amantino <andersonamantino@gmail.com>
2023-01-14 12:53:27 -0300

    Added exercício maior e média

---
+++ exercicios-java/exercicios/src/loops/MaiorEMedia.java
@@ -0,0 +1,35 @@
+package loops;
+
+import java.util.Scanner;
+
+/*
+ * Faça um programa que leia 5 números
+ * Informe o maior número
+ * Informe as médias dos números
+ * Usando o "do - while"
+ */
+
+public class MaiorEMedia {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);    
+
+        int numero;
+        int maiorNum = 0;
+        int mediaAccumulator = 0; 
+
+        int count = 0;
+        
+        do {
+            System.out.println("Digite uma nota (número): ");
+            numero = scan.nextInt();
+            count = count + 1;
+            mediaAccumulator = mediaAccumulator + numero;
+
+            if (numero > maiorNum) maiorNum = numero;
+
+        } while(count < 5); // 5 -> counter passar com o 0 uma vez
+        
+        System.out.println("O maior número (nota) foi: " + maiorNum);
+        System.out.println("Sua média foi: " + mediaAccumulator / 5);
+    }
+}
