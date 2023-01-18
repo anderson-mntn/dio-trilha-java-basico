bea4276ad80f9f6f9a252a2f23ea18fbe31f4e8d
anderson amantino <andersonamantino@gmail.com>
2023-01-18 14:21:07 -0300

    first commit

---
+++ exercicios-java/exercicios/src/codechallenges/Exercises.java
@@ -0,0 +1,16 @@
+package codechallenges;
+import java.util.Scanner;
+
+/*
+ * Algoritmo de radar para verificar se o motorista está na 
+ * velocidade máxima permitida (60km/h)
+ */
+
+public class Exercises {
+   public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int velocidadeAtual = leitor.nextInt();
+        if(velocidadeAtual <= 60) System.out.println("Nao foi multado");
+        else System.out.println("Foi multado");
+    }
+}
