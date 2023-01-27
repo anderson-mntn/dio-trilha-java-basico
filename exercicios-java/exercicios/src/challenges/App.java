e14ca86061ecfd76fb4259576a19366454bf4a88
anderson amantino <andersonamantino@gmail.com>
2023-01-27 15:01:03 -0300

    fix

--- exercicios-java/exercicios/src/challenges/App.java
+++ exercicios-java/exercicios/src/challenges/App.java
@@ -5,15 +5,7 @@ import java.io.NotSerializableException;
 import javax.swing.*;
 
 public class App {
-//     public static void main(String[] args) {
-//         kek(4, 5);
-//     }  
-//     public static void kek(int i, int y) {
-//         for(int j = 0; j < 5; j++){
-//         System.out.println(i * y); 
-//         i++;
-//         }    
-//    };
+
     public static void main(String[] args) {
         String a = JOptionPane.showInputDialog("Numerador: ");
         String b = JOptionPane.showInputDialog("Denominador: ");
